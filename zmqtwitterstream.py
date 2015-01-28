6d70d2c3b8daf3970ca3b5084a66f18edfae21b9
cmyr <colin@cmyr.net>
2015-01-27 20:49:49 -0500

    clean up main() and remove old refs

--- zmqtwitterstream.py
+++ zmqtwitterstream.py
@@ -127,18 +127,10 @@ class IterPublisher(object):
             self.backoff = min(self.backoff * 2, TWITTER_HTTP_MAX_BACKOFF)
 
 
-if __name__ == "__main__":
+def main():
     publisher = IterPublisher()
     publisher.run()
 
 
-# def main():
-# import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument('arg1', type=str, help="required argument")
-# parser.add_argument('arg2', '--argument-2', help='optional boolean argument', action="store_true")
-# args = parser.parse_args()
-
-
-# if __name__ == "__main__":
-# main()
+if __name__ == "__main__":
+    main()
