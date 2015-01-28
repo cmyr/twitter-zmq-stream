01a7b87a70e732bc19eaaa076cc4467d00524238
cmyr <colin@cmyr.net>
2015-01-28 11:14:00 -0500

    added host arg to stream consumer

--- zmqconsumer.py
+++ zmqconsumer.py
@@ -4,11 +4,11 @@ from __future__ import unicode_literals
 
 import zmq
 
-def zmq_iter(port):
+def zmq_iter(host="localhost", port=8069):
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
     socket.setsockopt_string(zmq.SUBSCRIBE, '')
-    socket.connect("tcp://localhost:%s" % port)
+    socket.connect("tcp://%s:%s" % (host, str(port)))
     while True:
         try:
             yield socket.recv_string()
@@ -18,19 +18,8 @@ def zmq_iter(port):
             break
 
 def main():
-    for msg in zmq_iter("8069"):
+    for msg in zmq_iter():
         print(msg)
 
 if __name__ == "__main__":
     main()
-
-# # def main():
-# #     import argparse
-# #     parser = argparse.ArgumentParser()
-# #     parser.add_argument('arg1', type=str, help="required argument")
-# #     parser.add_argument('arg2', '--argument-2', help='optional boolean argument', action="store_true")
-# #     args = parser.parse_args()
-
-
-# # if __name__ == "__main__":
-# #     main()
