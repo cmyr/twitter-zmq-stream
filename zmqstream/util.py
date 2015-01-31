f1bffc3d3d7340235ef87721fa58d213e3fd7305
cmyr <colin@cmyr.net>
2015-01-31 11:31:26 -0500

    activityindicator handles own timing

--- zmqstream/util.py
+++ zmqstream/util.py
@@ -3,16 +3,23 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import sys
+import time
 
 class ActivityIndicator(object):
     BULLET_FRAMES = ["■","◆","✦","★","✶","✷","✹","✺","◉","◎","◦","●","∙"]
     LINE_FRAMES = ["_","⎽","⎼","⎻","⎺","⎻","⎼","⎽"]
+    TRI_FRAMES = ["ᗑ","ᗒ","ᗐ","ᗕ","ᗅ","ᗆ","ᗄ","ᗉ","ᗋ","ᗌ","ᗊ","ᗏ","ᐃ","ᐅ","ᐁ","ᐊ","ᐄ","ᐓ","ᐍ","ᐗ"]
+    OLD_FRAMES = ["_", ",", ".", "•","*", "°", "ˆ", "´", "`", "¨"]
 
     """docstring for ActivityIndicator"""
-    def __init__(self, frames=None):
+    def __init__(self, frames=None, min_interval=0.1, message=""):
         super(ActivityIndicator, self).__init__()
-        self.indicatorFrames = frames or ["_", ",", ".", "•","*", "°", "ˆ", "´", "`", "¨"]
+        self.min_interval = min_interval
+        self.message = message
+        self.indicatorFrames = frames or ActivityIndicator.BULLET_FRAMES
         self.index = 0
+        self.last_tick = time.time()
+
 
     def __str__(self):
         return self.next()
@@ -23,19 +30,19 @@ class ActivityIndicator(object):
         return result
 
     def tick(self):
-        sys.stdout.write(" %s\r" % self.next())
-        sys.stdout.flush()
+        if time.time() - self.last_tick > self.min_interval:
+            self.last_tick = time.time()
+            sys.stdout.write(" %s %s\r" % (self.message, self.next()))
+            sys.stdout.flush()
 
 
 
 
 def main():
-    import time
 
     activity_indicator = ActivityIndicator(ActivityIndicator.LINE_FRAMES)
     while True:
         activity_indicator.tick()
-        time.sleep(0.2)
 
 
 if __name__ == "__main__":
