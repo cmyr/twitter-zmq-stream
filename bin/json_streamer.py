4341e32c45c52d5c145786e4a22b6c90bc97eef6
cmyr <colin@cmyr.net>
2016-04-02 16:59:52 -0400

    more verbose json_streamer

--- bin/json_streamer.py
+++ bin/json_streamer.py
@@ -10,26 +10,37 @@ import functools
 import time
 
 
-def make_delay_iter(an_iter, delay=0.01, request_kwargs={}):
+def make_delay_iter(an_iter, delay=0.01, langs=None, request_kwargs={}):
+    if langs:
+        langs = set(langs)
     for item in an_iter:
-        if item['lang'] == 'en':
+        if langs and item['lang'] in langs:
             yield StreamResult(StreamResultItem, item)
             time.sleep(delay)
 
 
 def main():
     import argparse
-    parser = argparse.ArgumentParser(description="Does something.")
-    parser.add_argument('source_dir', type=str, help="location of (folders of) json files")
-    parser.add_argument('--test', action='store_true', help='test json loading, printing to stdout')
+    parser = argparse.ArgumentParser(
+        description="iterates over gzip'd json'd files and streams contents")
+    parser.add_argument(
+        'source_dir', type=str, help="location of (folders of) json files")
+    parser.add_argument(
+        '-d', '--delay', type=float, default=0.01,
+        help='artificial delay between items')
+    parser.add_argument('--langs', type=str, nargs='*',
+                        help="only include tweets with these language codes")
+    parser.add_argument(
+        '--test', action='store_true', help='test json loading, printing to stdout')
     args = parser.parse_args()
 
     dir_items = io_utils.iter_dir(args.source_dir)
-    an_iter = functools.partial(make_delay_iter, dir_items)
+    an_iter = functools.partial(
+        make_delay_iter, dir_items, delay=args.delay, langs=args.langs)
 
     if args.test:
         for item in an_iter():
-            print(item)
+            print(item.value['lang'], item.value['text'])
     else:
         pub = StreamPublisher(
             iterator=an_iter)
